3f808bb69e0f07b4b05eaccae1d4f291267128e7
Nicolas HILAIRE <nicolas.hilaire@gmail.com>
2019-06-21 21:36:07 +0200

    stepper motor component finished

--- src/components/StepperMotor.vue
+++ src/components/StepperMotor.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
-    <svg preserveAspectRatio="xMidYMid meet" focusable="false" class="stepper-motor-position" >
-      <circle class="origin" :cx="_originOnCircleX" :cy="_originOnCircleY" r=10></circle>
+    <svg preserveAspectRatio="xMidYMid meet" focusable="false" class="stepper-motor-position" :style="_svgStyle" >
       <text class="positionValue" x="50%" y="50%" font-family="Verdana" font-size="35" text-anchor="middle" alignment-baseline="central">{{ position }}°</text>
-      <circle class="background" cx="50%" cy="50%" :r=radius transform="rotate(-90, 100,100)"></circle>
-      <circle  class="negative-amplitude" cx="50%" cy="50%" :r=radius transform="rotate(-90, 100,100)"
+      <circle class="background" cx="50%" cy="50%" :r=radius :transform="_circleRotation"></circle>
+      <circle  class="negative-amplitude" cx="50%" cy="50%" :r=radius :transform="_circleRotation"
             :style="{ strokeDasharray: _negativeDashArray }"></circle>
-      <circle  class="negative-amplitude" cx="50%" cy="50%" :r=radius transform="rotate(-90, 100,100)"
-            :style="{ strokeDasharray:'0, ' + _positiveStartDashValue + ', ' + _positiveStopDashValue + ', ' + _positiveRemainingDashValue }"></circle>
-      <circle class="cursor" :cx="_positionOnCircleX" :cy="_positionOnCircleY" r=10></circle>
+      <circle  class="positive-amplitude" cx="50%" cy="50%" :r=radius :transform="_circleRotation"
+            :style="{ strokeDasharray: _positiveDashArray }"></circle>
+      <circle class="origin" :cx="_originOnCircleX" :cy="_originOnCircleY"></circle>
+      <circle class="limit-cursor" :cx="_startCursorOnCircleX" :cy="_startCursorOnCircleY"></circle>
+      <circle class="limit-cursor" :cx="_endCursorOnCircleX" :cy="_endCursorOnCircleY"></circle>
+      <circle class="cursor" :cx="_positionOnCircleX" :cy="_positionOnCircleY"></circle>
     </svg>
   </div>
 </template>
@@ -18,6 +20,7 @@ export default {
   name: 'StepperMotor',
   props: ['radius', 'positiveAngle', 'negativeAngle', 'position', 'origin'],
   data: () => ({
+    margin: 20
   }),
   computed: {
 
@@ -27,54 +30,35 @@ export default {
 
     _negativeDashArray: function () {
       let array = []
+      let overlap = Math.max(0, this.circlePerimeter * (this.negativeAngle - this.origin) / 360)
       // we begin with a hole, so the first line end at 0px
       array.push(0)
       // we begin the line
       array.push(Math.max(0, this.circlePerimeter * (this.origin - this.negativeAngle) / 360))
       // we end the line at the origin
-      array.push(this.circlePerimeter * this.origin / 360)
+      array.push((this.circlePerimeter * this.negativeAngle / 360) - overlap)
+
       // we begin a hole until the overlap of the negative value on the positive angle
-      array.push(this.circlePerimeter - (this.circlePerimeter * this.negativeAngle / 360))
+      array.push(this.circlePerimeter - (this.circlePerimeter * this.origin / 360) - overlap)
       // we complete to end the circle with end of the overlap
-      array.push(Math.max(0, this.circlePerimeter * (this.negativeAngle - this.origin) / 360))
+      array.push(overlap)
       return array.join(',')
     },
 
     _positiveDashArray: function () {
       let array = []
       // we begin with a hole, so the first line end at 0px or with the overlap
-      array.push(Math.max(0, this.circlePerimeter * (360 - this.origin) / 360))
+      let overlap = Math.max(0, this.circlePerimeter * (this.origin + this.positiveAngle - 360) / 360)
+      array.push(overlap)
 
       // we begin the line at the origin
-      array.push(this.circlePerimeter * this.origin / 360)
+      array.push(this.circlePerimeter * this.origin / 360 - overlap)
       // we end the line at th en of the positive angle or at the end of the circle
-      array.push(Math.min(this.circlePerimeter, this.circlePerimeter * this.positiveAngle / 360))
+      array.push(this.circlePerimeter * this.positiveAngle / 360)
       // we add the remaining
       array.push(Math.max(0, this.circlePerimeter - (this.circlePerimeter * (this.origin + this.positiveAngle) / 360)))
 
-      // we complete to end the circle with end of the overlap
-      array.push(Math.max(0, this.circlePerimeter * (this.negativeAngle - this.origin) / 360))
       return array.join(',')
-    },
-
-    _negativeStopDashValue: function () {
-      return this.circlePerimeter * this.origin / 360
-    },
-
-    _negativeRemainingDashValue: function () {
-      return this.circlePerimeter - this._negativeStartDashValue - this._negativeStopDashValue
-    },
-
-    _positiveStartDashValue: function () {
-      return this.circlePerimeter * this.origin / 360
-    },
-
-    _positiveStopDashValue: function () {
-      return this.circlePerimeter * (this.origin + this.positiveAngle) / 360
-    },
-
-    _positiveRemainingDashValue: function () {
-      return this.circlePerimeter - this._positiveStartDashValue - this._positiveStopDashValue
     },
 
     _positionOnCircleX: function () {
@@ -91,6 +75,34 @@ export default {
 
     _originOnCircleY: function () {
       return Math.sin((this.origin - 90) * Math.PI / 180) * this.radius
+    },
+
+    _startCursorOnCircleX: function () {
+      return Math.cos((this.origin - this.negativeAngle - 90) * Math.PI / 180) * this.radius
+    },
+
+    _startCursorOnCircleY: function () {
+      return Math.sin((this.origin - this.negativeAngle - 90) * Math.PI / 180) * this.radius
+    },
+
+    _endCursorOnCircleX: function () {
+      return Math.cos((this.origin + this.positiveAngle - 90) * Math.PI / 180) * this.radius
+    },
+
+    _endCursorOnCircleY: function () {
+      return Math.sin((this.origin + this.positiveAngle - 90) * Math.PI / 180) * this.radius
+    },
+
+    _width: function () {
+      return this.radius * 2 + this.margin * 2
+    },
+
+    _svgStyle: function () {
+      return 'width: ' + this._width + 'px; height: ' + this._width + 'px;'
+    },
+
+    _circleRotation: function () {
+      return 'rotate(-90, ' + this._width / 2 + ',' + this._width / 2 + ')'
     }
   }
 }
@@ -98,19 +110,15 @@ export default {
 
 <style>
 
-.stepper-motor-position {
-  width: 200px; height: 200px;
-}
-
 .negative-amplitude {
-  stroke-width: 8px;
+  stroke-width: 15px;
   stroke: #ff5252;
   fill: none;
 }
 
 .positive-amplitude {
-  stroke-width: 8px;
-  stroke: green;
+  stroke-width: 15px;
+  stroke: #ff5252;
   fill: none;
 }
 
@@ -125,6 +133,7 @@ export default {
   stroke: green;
   fill: black;
   transform: translate(50%, 50%);
+  r: 10;
 }
 
 .origin {
@@ -132,6 +141,15 @@ export default {
   fill: #ff5252;
   stroke: #ff5252;
   transform: translate(50%, 50%);
+  r: 15;
+}
+
+.limit-cursor {
+  stroke-width: 0px;
+  fill: #ff5252;
+  stroke: #ff5252;
+  transform: translate(50%, 50%);
+  r: 15;
 }
 
 .positionValue {
