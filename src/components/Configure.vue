f01b25ec4db2d5d0a94feb196f857050423f610f
nicolas hilaire <nicolas.hilaire@carbonbee.fr>
2019-06-26 17:34:53 +0200

    wip on configuration vue

--- src/components/Configure.vue
+++ src/components/Configure.vue
@@ -8,7 +8,7 @@
       <md-card-content>
         <md-field>
           <label>Home Position Angle</label>
-          <md-input v-model="origin" type="number" min="0" max="360"></md-input>
+          <md-input v-model="origin" type="number" min="0" max="359"></md-input>
         </md-field>
         <md-field>
         <label>Counter clockwize angle</label>
@@ -22,7 +22,19 @@
           <label>Reduction rate</label>
           <md-input v-model="reductionRate" type="number" min="0"></md-input>
         </md-field>
+        <md-field>
+          <label for="stepResolution">Stepper motor resolution</label>
+          <md-select v-model="stepResolution">
+            <md-option v-for="stepResolutionOption in stepResolutionOptions" v-bind:key="stepResolutionOption.value" v-bind:value="stepResolutionOption.value">
+            {{stepResolutionOption.text}}
+            </md-option>
+          </md-select>
+        </md-field>
       </md-card-content>
+      <md-card-actions md-alignment="space-between">
+        <md-button class="md-raised md-accent">Restore default values</md-button>
+        <md-button>Save</md-button>
+      </md-card-actions>
     </md-card>
 
     <md-card>
@@ -52,7 +64,15 @@ export default {
       negativeAngle: 90.0,
       position: 23.0,
       origin: 0.0,
-      reductionRate: 6.0
+      reductionRate: 6.0,
+      stepResolution: 1,
+      stepResolutionOptions: [
+        { text: 'Full step', value: 1 },
+        { text: 'Half step', value: 0.5 },
+        { text: 'Quarter step', value: 0.25 },
+        { text: 'Eighth step', value: 0.125 },
+        { text: 'Sixteenth step', value: 0.125 }
+      ]
     }
   },
   components: {
